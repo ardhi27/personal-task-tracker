7c23dabf14fe4ec3909b2cbc97bd14cc41fb7717
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-08-25 14:44:12 +0800

    feat: Implemented route for home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import DisplayButton from "./DisplayButton";
 import InputPage from "./pages/input-task";
+import HomePage from "./pages/homepage";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<InputPage />}></Route>
+        <Route path="/homepage" element={<HomePage />}></Route>
       </Routes>
     </Router>
   );
